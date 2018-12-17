26cc5f5bf4975ed1b36d3af8ab909f9b01cf0785
Niema Moshiri <niemamoshiri@gmail.com>
2018-12-17 13:58:26 -0800

    Updated script to only check diagnosed people

--- scripts/optimal.py
+++ scripts/optimal.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from gzip import open as gopen
 from sys import argv
-if len(argv) != 4:
-    print("USAGE: %s <transmissions.txt.gz> <time> <n>"%argv[0]); exit()
-T = float(argv[2])
-N = int(argv[3])
+if len(argv) != 5:
+    print("USAGE: %s <diagnosis.txt.gz> <transmissions.txt.gz> <time> <n>"%argv[0]); exit()
+T = float(argv[3])
+PEOPLE = [l.split()[0].split('|')[1] for l in gopen(argv[1]).read().decode().strip().splitlines() if float(l.split()[1]) <= T]
+N = int(argv[4])
 num_infections = {}
-for line in gopen(argv[1]):
+for line in gopen(argv[2]):
     u,v,t = line.decode().strip().split('\t')
     if u not in num_infections:
         num_infections[u] = 0
