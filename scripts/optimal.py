944227ab2dd05145a2e1454802cdbdb1cc91c628
Niema Moshiri <niemamoshiri@gmail.com>
2018-12-26 15:11:09 -0800

    Fixed bug

--- scripts/optimal.py
+++ scripts/optimal.py
@@ -4,9 +4,9 @@ from sys import argv
 if len(argv) != 5:
     print("USAGE: %s <diagnosis.txt.gz> <transmissions.txt.gz> <time> <n>"%argv[0]); exit()
 T = float(argv[3])
-PEOPLE = [l.split()[0].split('|')[1] for l in gopen(argv[1]).read().decode().strip().splitlines() if float(l.split()[1]) <= T]
+PEOPLE = [l.split()[0].split('|')[1] for l in gopen(argv[1]).read().decode().strip().splitlines()]
 N = int(argv[4])
-num_infections = {}
+num_infections = {u:0 for u in PEOPLE}
 for line in gopen(argv[2]):
     u,v,t = line.decode().strip().split('\t')
     if u == 'None':
